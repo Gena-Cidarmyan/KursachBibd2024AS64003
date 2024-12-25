eb66fe7a29481c4849cd2cf5d7082cfb8cd37854
Gena-Cidarmyan <145318571+Gena-Cidarmyan@users.noreply.github.com>
2024-12-25 18:12:47 +0300

    Update and rename KursachBibdTogoEbal2024AS64003Ultra.cpp to KursachBibd2024AS64003.cpp

---
+++ KursachBibd2024AS64003.cpp
@@ -0,0 +1,12 @@
+﻿// KursachBibd2024AS64003.cpp: определяет точку входа для приложения.
+//
+
+#include "KursachBibd2024AS64003.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
